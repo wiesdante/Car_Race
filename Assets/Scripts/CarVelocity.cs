fe2c8cf6f9f58a1eafd8505bc79036bd611cb9f1
Mete Yıldırım <43517475+meteyldrm@users.noreply.github.com>
2022-07-26 01:39:36 +0300

    Add random car velocity

---
+++ Assets/Scripts/CarVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CarVelocity: MonoBehaviour {
+	private Rigidbody2D rb;
+	[SerializeField]
+	private Vector2 _time;
+	[SerializeField]
+	private Vector2 _velocity;
+	
+	IEnumerator RandomVelocity(Vector2 timeRange, Vector2 velocityRange) {
+		var time = Random.Range(timeRange[0], timeRange[1]);
+		var velocity = Random.Range(velocityRange[0], velocityRange[1]);
+		var v = rb.velocity;
+		v.y = velocity;
+		rb.velocity = v;
+		yield return new WaitForSeconds(time);
+		yield return StartCoroutine(RandomVelocity(timeRange, velocityRange));
+	}
+
+	private void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		StartCoroutine(RandomVelocity(_time, _velocity));
+	}
+}
